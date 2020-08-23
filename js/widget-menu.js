23211015ad7b317d948515e7cb7df79d0ac56061
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-08-23 14:46:28 +0300

    widget menu is hover now

--- js/widget-menu.js
+++ js/widget-menu.js
@@ -4,7 +4,23 @@ $(document).ready(function(){
   // about-widget-call
 
   let counter = 1;
-  $('#about-widget-call').click(function(){
+  // $('#about-widget-call').click(function(){
+    // counter++;
+    // switch (counter%2){
+    //   case 0:
+    //     $(".widget_about").addClass('widget_about_on');
+    //     $(".widget_about").removeClass('widget_about_off');
+    //     break;
+    //   default:
+    //     $(".widget_about").removeClass('widget_about_on');
+    //     $(".widget_about").addClass('widget_about_off');
+    //     break;
+    // }
+  // })
+
+  $('#about-widget-call').on('mouseover', function(){
+    // $(".widget_about").addClass('widget_about_on');
+    // $(".widget_about").removeClass('widget_about_off');
     counter++;
     switch (counter%2){
       case 0:
@@ -17,6 +33,23 @@ $(document).ready(function(){
         break;
     }
   })
+
+  $('section').on('mouseover', function(){
+    $(".widget_about").removeClass('widget_about_on');
+    $(".widget_about").addClass('widget_about_off');
+    counter = 1;
+  })
+
+  // $("#about-widget-call").on('mouseover', function(){
+  //   console.log('a')
+  // })
+
+  // $('.widget_about').on('mouseout', function(){
+  //   $(".widget_about").removeClass('widget_about_on');
+  //   $(".widget_about").addClass('widget_about_off');
+  // })
+
+
 
   $('#widget-list li').each(function(index){
     $(this).on('mouseover', function(){
