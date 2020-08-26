850e644d53689fbc4850ae8f32acc73a63c12652
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-08-26 13:45:58 +0300

    js jaw bugs fix

--- js/jaw.js
+++ js/jaw.js
@@ -27,53 +27,50 @@ $(document).ready(function(){
 
   })
 
-  $('#grey').click(function(){
-    $('body').css('background-color', 'rgb(106 126 138)');
-    $(deformELements).css('color', '#ffffff')
-    $('h2, h1, h3').css('color', '#ffffff')
-    $('.founder p').css('color', '#3D4B53')
-  })
+
+  let black = () => {
+    $('*').css('background-color', '#000000')
+    $('*').css('color', '#fff')
+  },
+  grey = () => {
+    $('*').css('background-color', 'rgb(106 126 138)')
+    $('*').css('color', '#fff')
+  },
+  green = () => {
+    $('*').css('background-color', '#9cfda7')
+    $('*').css('color', '#000')
+  },
+  blue = () => {
+    $('*').css('background-color', '#2C5DE5')
+    $('*').css('color', '#000')
+  },
+  normal = () => {
+    $('*').removeAttr("style");
+  }
 
   $('#black').click(function(){
-    $('body').css('background-color', '#000000');
-    $(deformELements).css('color', '#ffffff')
-    $('h2, h1, h3').css('color', '#ffffff')
-    // $('.founder').css('background-color', '#000000');
-    $('.founder p').css('color', '#3D4B53')
-    $('.FAQ .question').css('color', '#fff')
-    // $('section').css('background-color', '#000000')
-    // $('*').css('color', '#fff')
-    // $(this).siblings()
+    black();
+  })
+
+  $('#grey').click(function(){
+    grey();
   })
 
   $('#green').click(function(){
-    $('body').css('background-color', '#9cfda7');
-    $('.founder p').css('color', '#000')
+    green();
   })
 
   $('#blue').click(function(){
-    $('body').css('background-color', '#2C5DE5');
-    $(deformELements).css('color', '#fff')
-    $('h2, h1, h3').css('color', '#fff')
-    $('.founder p').css('color', '#3D4B53')
+    blue()
   })
 
   $('#come-back').click(function(){
-    $('body').css('background-color', '#fff');
-    $('p, span, a').css('color', '#3D4B53');
-    $('button').css('color', '#3D4B53');
-    $('p, span, input, select').css('font-size', '16px')
-    $('a, button').css('font-size', '14px')
-    $('.founder .info .role').css('font-size', '12px')
-    $('.founder .info .role').css('line-height', '16px')
-    $('h2, h3, h4').css('color', '#272B2D')
-    $('.FAQ h3').css('color', '#3D4B53')
-    $('.FAQ .question').css('color', '#3D4B53')
-    $('.subwrite, .title-pic').css('opacity', '1')
+    normal();
+    $('.section-title h2').css('opacity', '0.2')
   })
 
   $('#grey, #black, #green, #blue').click(function(){
-    $('.subwrite, .title-pic').css('opacity', '0')
+    $('.section-title h2').css('opacity', '0')
   })
 
 })
